b3316e5dcc28ad494f73647fccb80cd98d2d112d
Sidharth S <97397011+sidharthsajith@users.noreply.github.com>
2025-05-30 20:10:39 +0530

    feat: implement LinkedIn profile data extraction with Firecrawl API

--- scrape.py
+++ scrape.py
@@ -1,18 +1,28 @@
 from firecrawl import FirecrawlApp
 from pydantic import BaseModel, Field
 
-# Initialize the FirecrawlApp with your API key
-app = FirecrawlApp(api_key='your_api_key')
+
+# Initialize the client with your API key
+app = FirecrawlApp(api_key="fc-86571cce365c4b46bf438ad52d4afa49")
+
+# Perform a basic search
+search_result = app.search("AI Developer OR Machine Learning Engineer site:linkedin.com/in/ India", limit=100)
 
 class ExtractSchema(BaseModel):
-    company_mission: str
-    supports_sso: bool
-    is_open_source: bool
-    is_in_yc: bool
+    name: str = Field(description="Name of the person")
+    location: str = Field(description="Location of the person")
+    experience: str = Field(description="Experience of the person")
+    skills: list[str] = Field(description="Skills of the person")
+    past_experience: list[str] = Field(description="Past experience of the person")
+    
+    
+# Print the search results
+for result in search_result.data:
+    print(f"Title: {result['title']}")
+    print(f"URL: {result['url']}")
+    print(f"Description: {result['description']}")
 
-data = app.extract([
-  'https://docs.firecrawl.dev/*', 
-  'https://firecrawl.dev/', 
-  'https://www.ycombinator.com/companies/'
-], prompt='Extract the company mission, whether it supports SSO, whether it is open source, and whether it is in Y Combinator from the page.', schema=ExtractSchema.model_json_schema())
+for i in search_result.data:
+    print(i['url'])
+    data = app.extract((i['url']), prompt='Extract the name, location, experience, skills, and past experience of the person from the page.', schema=ExtractSchema.model_json_schema())
 print(data)
